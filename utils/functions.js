aa8b9ae16d0adc33bf23191eae7456ab088aea22
Felipe Queis <fqueis@gmail.com>
2021-04-28 22:11:56 -0300

    refactor code

--- utils/functions.js
+++ utils/functions.js
@@ -1,6 +1,11 @@
+'use strict'
+
+const fs = require('fs')
+
 module.exports = {
     asyncForEach: async (array, callback) => {
         for (let index = 0; index < array.length; index++)
             await callback(array[index], index, array);
-    }
+    },
+    writeResults: async (file, content, callback) => fs.writeFile(file, JSON.stringify(content, null, 2), callback)
 }
